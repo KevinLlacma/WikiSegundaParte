57cdd66b83b1037ab15737079c88b0ed1e27c499
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 01:06:54 -0500

    Se hace la consulta

--- login.pl
+++ login.pl
@@ -7,9 +7,13 @@ use DBI;
 my $q=CGI->new;
 print $q->header("text/XML");
 
+my $user = $q->param("user");
+my $password = $q->param("password");
+my $userB = "alumno";
+my $passwordB = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
+my $dbh = DBI->connect($dsn, $userB, $passwordB) or die ("No se ha podido establecer la conexion");
 
- my $user = "alumno";
- my $password = 'pweb1';
- my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
- my $dbh = DBI->connect($dsn, $user, $password) or die ("No se ha podido establecer la conexion");
+my $sth = $dbh->prepare("SELECT userName, firstName, lastName FROM users WHERE userName = ? AND password = ?");
+$sth->execute($user, $password);
 
