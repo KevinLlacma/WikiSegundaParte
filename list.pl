5f38557e8243ce3d6b57ef4d722120d2a4ec8aed
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 00:37:44 -0500

    Se agrego archivo list.pl y conexion a la tabla de datos

---
+++ list.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+my $q = CGI->new;
+print $q->header("text/XML");
+my $owner = $q->param("owner");
+
+my $user = "alumno";
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
+my $dbh = DBI->connect($dsn, $user, $password) or die ("No se pudo ha podido establecer conexion");
+
