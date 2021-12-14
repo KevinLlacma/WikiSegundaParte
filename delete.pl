6d5def1984a38c6f956cc876f916b233e1f5d650
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 01:27:11 -0500

    Se hace la consulta para borrar

--- delete.pl
+++ delete.pl
@@ -12,4 +12,11 @@ my $password = 'pweb1';
 my $owner = $q->param("owner");
 my $title = $q->param("title");
 my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
-my $dbh = DBI->connect($dsn, $user, $password) or die ("No se pudo conectar");
+my $dbh = DBI->connect($dsn, $user, $password) or die ("No se ha podido establecer conexion");
+my $sth = $dbh->prepare("DELETE FROM Articles WHERE owner = ? AND title = ?");
+$sth->execute($owner, $title);
+$sth->finish;
+$dbh->disconnect;
+
+
+
