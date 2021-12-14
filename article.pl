c568418d3a72c6bdf1ea79184d93b1a86c740f5a
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 02:56:05 -0500

    Salida XML

--- article.pl
+++ article.pl
@@ -13,3 +13,17 @@ my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
 my $dbh = DBI->connect($dsn, $user, $password) or die ("No se ha podido establecer conexion");
 my $sth = $dbh->prepare("SELECT owner, title, text FROM Articles WHERE owner = ? and title = ?");
 $sth->execute($owner, $title);
+print $q->header("text/XML");
+
+while (my @arreglo = $sth->fetchrow_array){
+  print <<BLOCK;
+  <?xml version='1.0' encoding='utf-8'?>
+   <article>
+    <owner>$owner</owner>
+    <title>$arreglo[0]</title>
+    <text>$arreglo[1]</text>
+   </article>
+  BLOCK
+} 
+  $sth->finish;
+  $dbh->disconnect;
