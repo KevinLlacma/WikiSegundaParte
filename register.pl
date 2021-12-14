e7d9c9a84bcd1ab27a1b26a545baa1cb21a103e6
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 02:22:41 -0500

    Resgistro de la consulta

--- register.pl
+++ register.pl
@@ -7,14 +7,24 @@ use DBI;
 my $q = CGI->new;
 print $q->header("text/XML");
 
-my $user= "alumno";
-my $password= 'pweb1';
+my $userB= "alumno";
+my $passwordB= 'pweb1';
 my $userName = $q->param("userName");
 my $password = $q->param("password");
 my $firstName = $q->param("firstName");
 my $lastName = $q->param("lastName");
+my $esp = 1;
 my $dsn= "DBI:MariaDB:database=pweb1;host=192.168.1.69";
-my $dbh= DBI->connect($dsn, $user, $password) or die ("No se ha podido establecer conectar");
+my $dbh= DBI->connect($dsn, $userB, $passwordB) or die ("No se ha podido establecer conectar");
+my @arreglo;
+my $sth = $dbh->prepare("SELECT * FROM Users WHERE userName = ?"); 
+$sth->execute($userName);
+if (@rreglo = $sth->fetchrow_array){
+    $esp = 0;
+}
 
+print "<?xml version='1.0' encoding='UTF-8'?>\n";
 
-
+if ($esp == 1){
+my $sth = $dbh->prepare("INSERT INTO Users (userName, password, lastName, firstName) VALUES (?,?,?,?)");
+$sth->execute($userName, $password, $lastName, $firstName);
