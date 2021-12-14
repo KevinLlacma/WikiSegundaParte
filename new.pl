c47061a0df7b6b74a38c967aae8b55f5d7323bcb
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 02:28:25 -0500

    si existe usuarios y titulos

--- new.pl
+++ new.pl
@@ -14,4 +14,16 @@ my $user = "alumno";
 my $password = 'pweb1';
 my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
 my $dbh = DBI->connect($dsn, $user, $password) or die ("No se pudo ha podido establecer conexion");
-
+my $sth = $dbh->prepare("SELECT * FROM users WHERE userName = ?");
+$sth->execute($owner);
+my $titulos = 0;
+my $usuarios = 0;
+if(my @arreglo = $sth->fetchrow_array){
+ $usuarios = 1;
+}
+$sth = $dbh->prepare("SELECT * FROM Articles WHERE owner = ? AND title = ?");
+$sth->execute($owner, $title); 
+if(my @arreglo = $sth->fetchrow_array){
+ $titulos = 1;
+}
+  
