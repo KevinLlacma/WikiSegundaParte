cfbd6b3e5c7a3104a7292275804a91445636499c
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 00:46:42 -0500

    Se agrego archivo delte.pl mas conexion a la base de datos

---
+++ delete.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+my $q = CGI->new;
+print $q->header("text/XML"); 
+
+my $user = "alumno";
+my $password = 'pweb1';
+my $owner = $q->param("owner");
+my $title = $q->param("title");
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
+my $dbh = DBI->connect($dsn, $user, $password) or die ("No se pudo conectar");
