316af1b8ca3fbb8f3b3dee274047c6618e306ee8
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 03:16:26 -0500

    Salida XML

--- new.pl
+++ new.pl
@@ -14,7 +14,7 @@ my $user = "alumno";
 my $password = 'pweb1';
 my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
 my $dbh = DBI->connect($dsn, $user, $password) or die ("No se pudo ha podido establecer conexion");
-my $sth = $dbh->prepare("SELECT * FROM users WHERE userName = ?");
+my $sth = $dbh->prepare("SELECT * FROM Users WHERE userName = ?");
 $sth->execute($owner);
 my $titulos = 0;
 my $usuarios = 0;
@@ -26,4 +26,25 @@ $sth->execute($owner, $title);
 if(my @arreglo = $sth->fetchrow_array){
  $titulos = 1;
 }
-  
+
+print "<?xml version='1.0' encoding='UTF-8'?>\n";
+
+if (($usuarios==1)&&($titulos==0)){
+  $sth = $dbh->prepare("INSERT INTO Articles (owner, title, text) VALUES (?,?,?)");
+  $sth->execute($owner, $title, $text);
+  $sth = $dbh->prepare("SELECT title, text FROM Articles WHERE owner = ? AND title = ?");
+  $sth->execute($owner, $title);
+          
+   if (my @arreglo=$sth->fetchrow_array){
+     print "<article>\n";
+     print "<title>$arreglo[0]</title>\n";
+     print "<text>$arreglo[1]</text>\n";
+     print "</article>\n";
+   } 
+} 
+else {
+  print "  <article></article>\n";
+}
+$sth->finish;
+$dbh->disconnect;
+
