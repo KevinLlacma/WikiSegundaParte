69beabf048f0e81aff81ebbe55befb1f584c3239
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 02:59:33 -0500

    Salida en XML

--- update.pl
+++ update.pl
@@ -16,3 +16,14 @@ my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.69";
 my $dbh = DBI->connect($dsn, $user, $password) or die ("No se ha podido establecer conexion");
 my $sth = $dbh->prepare("UPDATE Articles SET text = ? WHERE owner = ? AND title = ?");
 $sth->execute($text, $owner, $title);
+$sth= $dbh->prepare("SELECT title, text FROM Articles WHERE owner = ? AND title = ?");
+$sth->execute($owner, $title);
+print "<?xml version='1.0' encoding='UTF-8'?>\n";
+if (my @arreglo = $sth->fetchrow_array){
+    print "<article>\n";
+    print "<title>$arreglo[0]</title>\n";
+    print "<text>$arreglo[1]</text>\n";
+    print "</article>\n";
+}
+$sth->finish;
+$dbh->disconnect;
