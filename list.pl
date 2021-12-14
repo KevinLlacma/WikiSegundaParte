e3ff9b75db6bee9623525eeac105c861492b3a28
KevinLlacma <kllacma@unsa.edu.pe>
2021-12-14 02:32:20 -0500

    Salida en XML

--- list.pl
+++ list.pl
@@ -15,3 +15,15 @@ my $dbh = DBI->connect($dsn, $user, $password) or die ("No se pudo ha podido est
 
 my $sth = $dbh->prepare("SELECT owner, title FROM Articles WHERE owner = ?");
 $sth->execute($owner);
+
+print "<?xml version='1.0' encoding='UTF-8'?>\n";
+print "<articles>\n";
+while (my @arreglo = $sth->fetchrow_array){
+  print "  <article>\n";
+  print "    <owner>$arreglo[0]</owner>\n";
+  print "    <title>$arreglo[1]</title>\n";
+  print "  </article>\n";
+}
+print "</articles>\n";
+$sth->finish;
+$dbh->disconnect;
